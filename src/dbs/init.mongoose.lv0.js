d17c858d69f94335662ee42b52c23cbb47f441f1
noname21-stack <vanhuybuivips@gmail.com>
2023-08-08 09:42:10 +0700

    section_2

---
+++ src/dbs/init.mongoose.lv0.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const connectString ="mongodb://localhost:27017/shopDev"
+
+mongoose.connect(connectString)
+    .then(_=>{
+        console.log("Connect Mongoose Success")
+    })
+    .catch( err =>{
+        console.log("Error Connect!");
+    })
+
+
+    //dev
+if(1 === 1){
+    mongoose.set('debug', true);
+    mongoose.set('debug', {color : true});
+}
+
+module.exports =mongoose;
